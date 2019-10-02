3823e58b6415046aef4e5a70e13b27de131fc0c9
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-02 20:22:27 +1300

    Update createSql.py

--- createSql.py
+++ createSql.py
@@ -2,7 +2,7 @@ import os
 
 # 创建数据库，google sql
 os.system("gcloud sql tiers list")
-os.system("gcloud sql instances create my-moodle --tier=db-n1-standard-2 --region=us-central1-a")
+os.system("gcloud sql instances create my-moodle --tier=db-n1-standard-2 --region=us-central1")
 os.system("gcloud sql instances patch --assign-ip my-moodle")
 os.system("gcloud sql users set-password root --host % --instance my-moodle --password Moodle123moodle")
 
