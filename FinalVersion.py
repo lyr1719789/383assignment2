d17a8779e55b3acfe9f817176259e0575b72ec64
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-02 20:04:02 +1300

    Update FinalVersion.py

--- FinalVersion.py
+++ FinalVersion.py
@@ -19,34 +19,39 @@ os.system("gcloud sql instances patch my-moodle --authorized-networks=%s")%manua
 os.system("wget https://files.phpmyadmin.net/phpMyAdmin/4.6.3/phpMyAdmin-4.6.3-all-languages.tar.bz2 ")
 os.system("mkdir phpMyAdmin")
 os.system("tar -xvf phpMyAdmin-4.6.3-all-languages.tar.bz2 -C phpMyAdmin --strip-components=1")
+pathOfphp = os.path.abspath('phpMyAdmin')
 
 # create app.yaml
-os.chdir("/phpMyAdmin")
+os.chdir(pathOfphp)
 os.system("sudo touch app.yaml")
-os.system("sudo chmod o+r /phpMyAdmin/app.yaml")
-os.system("sudo chmod o+w /phpMyAdmin/app.yaml")
+pathOfapp = os.path.abspath('app.yaml')
+os.chdir(pathOfapp)
+os.system("sudo chmod o+r app.yaml")
+os.system("sudo chmod o+w app.yaml")
 yaml="service: default\nruntime: php55\napi_version: 1\n\nhandlers:\n\n- url: /(.+\.(ico|jpg|png|gif))$\n  static_files: \\1\n  upload: (.+\.(ico|jpg|png|gif))$\n  application_readable: true\n\n- url: /(.+\.(htm|html|css|js))$\n  static_files: \\1\n  upload: (.+\.(htm|html|css|js))$\n  application_readable: true\n\n- url: /(.+\.php)$\n  script: \\1\n  login: admin\n\n- url: /.*\n  script: index.php\n  login: admin"
-Modified_file_1 = open('/phpMyAdmin/app.yaml',"w")
+Modified_file_1 = open(pathOfapp,"w")
 Modified_file_1.write(yaml)
 Modified_file_1.close()
 
 # create config.inc.php
-os.chdir("/phpMyAdmin")
+os.chdir(pathOfphp)
 os.system("touch config.inc.php")
-os.system("sudo chmod o+r /phpMyAdmin/config.inc.php")
-os.system("sudo chmod o+w /phpMyAdmin/config.inc.php")
+os.system("sudo chmod o+r config.inc.php")
+os.system("sudo chmod o+w config.inc.php")
+pathOfconfig = os.path.abspath('config.inc.php')
 config="<?php\n$cfg[\'blowfish_secret\'] = 'phpMyAdmin';\n\n$i = 0;\n\n// Change this to use the project and instance that you've created.\n$host = '/cloudsql/%s';\n$type = 'socket';\n\n$i++;\n\n$cfg['Servers'][$i]['auth_type'] = 'cookie';\n\n$cfg['Servers'][$i]['socket'] = $host;\n$cfg['Servers'][$i]['connect_type'] = $type;\n$cfg['Servers'][$i]['compress'] = false;\n\n$cfg['Servers'][$i]['extension'] = 'mysqli';\n$cfg['Servers'][$i]['AllowNoPassword'] = true;\n\n$cfg['UploadDir'] = '';\n$cfg['SaveDir'] = '';\n\n$cfg['PmaNoRelation_DisableWarning'] = true;\n$cfg['ExecTimeLimit'] = 60;\n$cfg['CheckConfigurationPermissions'] = false;"%manual.sql_name
-Modified_file_2 = open('/phpMyAdmin/config.inc.php',"w")
+Modified_file_2 = open(pathOfconfig,"w")
 Modified_file_2.write(config)
 Modified_file_2.close()
 
 # create php.ini
-os.chdir("/phpMyAdmin")
+os.chdir(pathOfphp)
 os.system("touch php.ini")
-os.system("sudo chmod o+r /phpMyAdmin/php.ini")
-os.system("sudo chmod o+w /phpMyAdmin/php.ini")
+os.system("sudo chmod o+r php.ini")
+os.system("sudo chmod o+w php.ini")
+pathOfini = os.path.abspath('php.ini')
 php="google_app_engine.enable_functions = \"php_uname, getmypid\""
-Modified_file_3 = open('/phpMyAdmin/php.ini',"w")
+Modified_file_3 = open(pathOfini,"w")
 Modified_file_3.write(php)
 Modified_file_3.close()
 
