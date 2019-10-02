1c4a917b37e88a80f544c506d5a931dfc870b78c
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-02 21:14:00 +1300

    Update manual.py

--- manual.py
+++ manual.py
@@ -1,4 +1,5 @@
 global ip,host_ip,sql_name
+project = input("your project name:")
 ip = input("your instance static external ip:")
-host_ip = input("your google cloud sql host ip :")
+host_ip = input("your google cloud sql host ip:")
 sql_name = input("your google cloud sql instance name is:")
