9e09a11c1ccd75cb0bb884a049573ee405078da8
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-03 17:07:22 +1300

    Update FinalVersion.py

--- FinalVersion.py
+++ FinalVersion.py
@@ -21,6 +21,9 @@ os.system("mkdir phpMyAdmin")
 os.system("tar -xvf phpMyAdmin-4.6.3-all-languages.tar.bz2 -C phpMyAdmin --strip-components=1")
 pathOfphp = os.path.abspath('phpMyAdmin')
 
+#启用app engune
+os.system("gcloud app create")
+
 # create app.yaml
 os.chdir(pathOfphp)
 os.system("sudo touch app.yaml")
