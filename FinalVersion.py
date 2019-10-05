c7b0e638bed9ab05d253e9dd29cd5a3aada3d28f
lyr1719789 <44421919+lyr1719789@users.noreply.github.com>
2019-10-05 15:59:47 +1300

    Update FinalVersion.py

--- FinalVersion.py
+++ FinalVersion.py
@@ -7,6 +7,10 @@ os.system("gcloud compute instances delete-access-config deployment-vm --access-
 os.system("gcloud compute instances add-access-config deployment-vm --access-config-name \"External NAT\" --address %s"%manual.ip)
 print("vm instance finished")
 
+os.system("gcloud compute --project=%s firewall-rules create for-apache --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0"%manual.project)
+
+
+
 # 创建磁盘 --project=projectname,create disk
 os.system("gcloud beta compute disks create backup --project=%s --type=pd-standard --size=500GB --zone=us-central1-a --physical-block-size=4096"%manual.project)
 # 连接磁盘 --disk=diskname, connect disk
